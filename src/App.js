8d17bab9d475989ff99ddaf19a563ab99cb943a3
Abhishek Sharma <as327209@gmail.com>
2025-01-03 21:18:37 +0530

    dashboards and app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Link, Navigate, useNavigate } from 'react-router-dom';
-import Home from './pages/Home';
-import GTranslate from './components/GTranslate';
-// import ConsumerDB from './pages/user/Dashboard';
-// import Profile from './pages/profile'
-import CompanyProfile from './pages/company/CompanyProfile';
 import { toast } from 'react-toastify';
 import { auth } from './pages/firebase';
-import { AuthProvider } from './context/AuthContext';
+import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import ProducerDB from './pages/producer/Dashboard';
+import CompaniesDB from './pages/company/Dashboard';
 import ConsumerDB from './pages/user/Dashboard';
-import CompanyDashboard from './pages/company/Dashboard';
+// import Profile from './pages/profile'
+import CompanyProfile from './pages/company/CompanyProfile';
+import ProducerProfile from './pages/producer/ProducerProfile';
+import GTranslate from './components/GTranslate';
+import { AuthProvider } from './context/AuthContext';
 
 // Protected Route Component
 const ProtectedRoute = ({ children }) => {
@@ -44,10 +45,46 @@ const ProtectedRoute = ({ children }) => {
   return authenticated ? children : null;
 };
 
-
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userType, setUserType] = useState(null);
+  // useEffect(() => {
+  //   // Inject Naker.io script dynamically
+  //   const script = document.createElement("script");
+  //   script.setAttribute("data-who", "💎 Made with naker.io 💎");
+  //   script.src =
+  //     "https://d23jutsnau9x47.cloudfront.net/back/v1.0.9/viewer.js";
+  //   script.setAttribute(
+  //     "data-option",
+  //     JSON.stringify({
+  //       environment: {
+  //         gradient: "radial",
+  //         sensitivity: 0.8,
+  //         colorStart: [59,130,246,1],
+  //         colorEnd: [68,188,112,1],
+  //       },
+  //       particle: {
+  //         life: 5,
+  //         power: 0.045,
+  //         texture:
+  //           "https://res.cloudinary.com/naker-io/image/upload/v1566560053/circle_05.png",
+  //         number: 101,
+  //         colorStart: [116, 129, 92, 0.13],
+  //         colorEnd: [198,188,107,0.94],
+  //         sizeStart: 1.57,
+  //         sizeEnd: 3.14,
+  //         direction1: { x: 100, y: 100, z: 100 },
+  //         direction2: { x: -100, y: -100, z: -100 },
+  //       },
+  //     })
+  //   );
+  //   document.body.appendChild(script);
+
+  //   return () => {
+  //     // Cleanup script on component unmount
+  //     document.body.removeChild(script);
+  //   };
+  // }, []);
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -59,11 +96,30 @@ const App = () => {
 
   return (
     <AuthProvider>
-      <div className="profile-container">
-      <div className="content">
+    <div className="profile-container">
+    <div className="content">
     <BrowserRouter>
     <GTranslate/>
-      
+      {/* <nav className="p-4 bg-gray-100 shadow-sm">
+        <ul className="flex space-x-4">
+        
+          {isLoggedIn && userType === 'producer' && (
+            <li>
+              <Link to="/producer" className="text-blue-500 hover:underline">Producer Dashboard</Link>
+            </li>
+          )}
+          {isLoggedIn && userType === 'company' && (
+            <li>
+              <Link to="/company" className="text-blue-500 hover:underline">Company Dashboard</Link>
+            </li>
+          )}
+          {isLoggedIn && userType === 'user' && (
+            <li>
+              <Link to="/user" className="text-blue-500 hover:underline">User Dashboard</Link>
+            </li>
+          )}
+        </ul>
+      </nav> */}
 
       <Routes>
         <Route path="/" element={<Home />} />
@@ -78,10 +134,28 @@ const App = () => {
         />
         <Route path="/register" element={<Register/>} />
         <Route
+          path="/producerDB"
+          element={
+            <ProtectedRoute>
+              <ProducerDB />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+        path='/producerprofile' element={<ProducerProfile/>}
+        />
+         <Route
+          path="*"
+          element={
+            <Navigate to="/" replace />
+          }
+        />
+
+        <Route
           path="/companiesDB"
           element={
             <ProtectedRoute>
-              <CompanyDashboard />
+              <CompaniesDB />
             </ProtectedRoute>
           }
         />
@@ -102,9 +176,13 @@ const App = () => {
             </ProtectedRoute>
           }
         />
+        
+       
 
       </Routes>
+     
     </BrowserRouter>
+    
     </div>
     </div>
     </AuthProvider>
